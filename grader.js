ef430cdd52359df3b31c0ae6966e708dc4d5c3c4
lewa <lee.wasilenko@anevis-solutions.com>
2013-07-10 08:03:22 -0700

    submit version. HTML download still buggy

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "NONE";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,8 +68,25 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to get index.html', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson;
+    if( (program.file === HTMLFILE_DEFAULT) && (program.url !== URL_DEFAULT) ) {
+        var indexFromWeb = "./index.html.from.url";
+	rest.get(program.url).on('complete', function(result, response) {
+	  if(result instanceof Error) {
+	      console.error('Error: '+ util.format(response.message));
+	  } else {
+	      console.error("Wrote %s", indexFromWeb);
+	      fs.writeFileSync(indexFromWeb, result);
+	  }
+      });
+	checkJson= checkHtmlFile(indexFromWeb, program.checks);
+    } else if ( (program.file !== HTMLFILE_DEFAULT) && (program.url === URL_DEFAULT) ) {
+      checkJson = checkHtmlFile(program.file, program.checks);
+    } else {
+      checkJson = checkHtmlFile(program.file, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
